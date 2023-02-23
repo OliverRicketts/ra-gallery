199189ab6eba502cef23db7035e4256d6a9c0b9d
OliverRicketts <126100528+OliverRicketts@users.noreply.github.com>
2023-02-23 22:55:09 +0000

    Update gallery1.php

--- gallery1.php
+++ gallery1.php
@@ -1,85 +1,96 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>Roger Ricketts Gallery</title>
-    <style>
-      .gallery {
-        display: grid;
-        grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-        grid-gap: 10px;
-      }
-      
-      .gallery img {
-        width: 100%;
-        height: auto;
-        object-fit: cover;
-        cursor: pointer;
-        transition: transform 0.3s ease-in-out;
-      }
-      
-      .modal {
-        display: none;
-        position: fixed;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background-color: rgba(0, 0, 0, 0.8);
-      }
-      
-      .modal-content {
-        display: block;
-        max-width: 80%;
-        max-height: 80%;
-        margin: auto;
-      }
-      
-      .close {
-        color: #fff;
-        font-size: 24px;
-        font-weight: bold;
-        position: absolute;
-        top: 10px;
-        right: 20px;
-        cursor: pointer;
-      }
+<head>
+    <title>Roger Ricketts Art Gallery</title>
+    <style type="text/css">
+        body {
+            background-color: #f5f5f5;
+            font-family: Arial, sans-serif;
+        }
+
+        .gallery {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+            align-items: center;
+            margin: 0 auto;
+            max-width: 1200px;
+        }
+
+        .gallery img {
+            width: 100%;
+            height: auto;
+            object-fit: contain;
+            transition: transform .5s ease;
+            cursor: pointer;
+        }
+
+        .gallery img.active {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            z-index: 999;
+            max-width: 100%;
+            max-height: 100%;
+        }
+
+        @media screen and (min-width: 768px) {
+            .gallery img {
+                width: auto;
+                max-height: 200px;
+                margin: 5px;
+            }
+        }
     </style>
-  </head>
-  <body>
-    <div class="gallery">
-      <?php
-        $directory = "files/artwork";
-        $images = glob($directory . "/*.png");
-        foreach($images as $image) {
-          echo '<img src="' . $image . '" onclick="openModal(this)">';
+</head>
+<body>
+<div class="gallery">
+    <?php
+    $dir = "files/artwork/";
+    $images = glob($dir . "*.png");
+    foreach ($images as $image):
+        ?>
+        <img src="<?php echo $image; ?>" alt="" onclick="expandImage(this)">
+    <?php endforeach; ?>
+</div>
+<script type="text/javascript">
+    let images = document.querySelectorAll('.gallery img');
+    let activeImageIndex = 0;
+
+    document.addEventListener('keydown', function (event) {
+        if (event.keyCode === 37) {
+            navigate(-1);
+        } else if (event.keyCode === 39) {
+            navigate(1);
         }
-      ?>
-    </div>
-    
-    <div id="myModal" class="modal">
-      <span class="close" onclick="closeModal()">&times;</span>
-      <img class="modal-content" id="modalImage">
-    </div>
-    
-    <script>
-      function openModal(img) {
-        var modal = document.getElementById("myModal");
-        var modalImg = document.getElementById("modalImage");
-        modal.style.display = "block";
-        modalImg.src = img.src;
-        img.style.transform = "scale(1.2)";
-      }
-      
-      function closeModal() {
-        var modal = document.getElementById("myModal");
-        var modalImg = document.getElementById("modalImage");
-        modal.style.display = "none";
-        modalImg.src = "";
-        var galleryImages = document.getElementsByClassName("gallery")[0].getElementsByTagName("img");
-        for (var i = 0; i < galleryImages.length; i++) {
-          galleryImages[i].style.transform = "scale(1)";
+    });
+
+    function expandImage(image) {
+        image.classList.add('active');
+        activeImageIndex = Array.from(images).indexOf(image);
+    }
+
+    function shrinkImage() {
+        images[activeImageIndex].classList.remove('active');
+    }
+
+    function navigate(direction) {
+        shrinkImage();
+
+        activeImageIndex += direction;
+        if (activeImageIndex < 0) {
+            activeImageIndex = images.length - 1;
+        } else if (activeImageIndex >= images.length) {
+            activeImageIndex = 0;
         }
-      }
-    </script>
-  </body>
+
+        images[activeImageIndex].classList.add('active');
+        images[activeImageIndex].addEventListener('transitionend', function () {
+            images[activeImageIndex].removeEventListener('transitionend', this);
+            isTransitioning = false;
+        });
+    }
+</script>
+</body>
 </html>
